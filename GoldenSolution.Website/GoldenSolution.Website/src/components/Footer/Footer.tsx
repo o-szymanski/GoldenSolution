eeaeef9716d6da12f23a7b350ac74081d62427b6
o-szymanski <osk.szymanski@gmail.com>
2024-02-19 22:10:05 +0100

    add logo

--- GoldenSolution.Website/GoldenSolution.Website/src/components/Footer/Footer.tsx
+++ GoldenSolution.Website/GoldenSolution.Website/src/components/Footer/Footer.tsx
@@ -6,25 +6,26 @@ import youtube from "../../assets/socials/youtube.svg"
 import twitter from "../../assets/socials/twitter.svg"
 import linkedin from "../../assets/socials/linkedin-in.svg"
 import mail from "../../assets/socials/envelope-solid.svg"
+import logoImage from "../../assets/socials/logo.svg"
 
-function Footer() :React.ReactElement {
-    const socialMedia :React.ReactElement = <div className={styles.footerSocialMedia}>
-        <span className={styles.footerLine}><hr/></span>
-        <span className={styles.footerSocialMediaItem}><a href="#"><img alt="Golden Solution Facebook" src={facebook}/></a></span>
-        <span className={styles.footerSocialMediaItem}><a href="#"><img alt="Golden Solution Instagram" src={instagram}/></a></span>
-        <span className={styles.footerSocialMediaItem}><a href="#"><img alt="Golden Solution YouTube" src={youtube}/></a></span>
-        <span className={styles.footerSocialMediaItem}><a href="#"><img alt="Golden Solution X/Twitter" src={twitter}/></a></span>
-        <span className={styles.footerSocialMediaItem}><a href="#"><img alt="Golden Solution LinkedIn" src={linkedin}/></a></span>
-        <span className={styles.footerSocialMediaItem}><a href="#"><img alt="Golden Solution Mail" src={mail}/></a></span>
-        <span className={styles.footerLine}><hr/></span>
+function Footer(): React.ReactElement {
+    const socialMedia: React.ReactElement = <div className={styles.footerSocialMedia}>
+        <span className={styles.footerLine}><hr /></span>
+        <span className={styles.footerSocialMediaItem}><a href="#"><img alt="Golden Solution Facebook" src={facebook} /></a></span>
+        <span className={styles.footerSocialMediaItem}><a href="#"><img alt="Golden Solution Instagram" src={instagram} /></a></span>
+        <span className={styles.footerSocialMediaItem}><a href="#"><img alt="Golden Solution YouTube" src={youtube} /></a></span>
+        <span className={styles.footerSocialMediaItem}><a href="#"><img alt="Golden Solution X/Twitter" src={twitter} /></a></span>
+        <span className={styles.footerSocialMediaItem}><a href="#"><img alt="Golden Solution LinkedIn" src={linkedin} /></a></span>
+        <span className={styles.footerSocialMediaItem}><a href="#"><img alt="Golden Solution Mail" src={mail} /></a></span>
+        <span className={styles.footerLine}><hr /></span>
     </div>
     const logo: React.ReactElement = <div className={styles.footerLogo}>
-        <img alt="Golden Solution footer logo" src="https://via.placeholder.com/150" className={styles.footerLogoImage}></img>
+        <img alt="Golden Solution Logo" src={logoImage} className={styles.footerLogoImage}></img>
     </div>
-    const copyright :React.ReactElement = <div className={styles.footerCopyright}>
+    const copyright: React.ReactElement = <div className={styles.footerCopyright}>
         <p>Copyright &copy; {new Date().getFullYear()} Golden Solution</p>
     </div>
-    const siteMap :React.ReactElement = <div className={styles.footerSiteMap}>
+    const siteMap: React.ReactElement = <div className={styles.footerSiteMap}>
         <span><a href="#">Home</a></span>
         <span><a href="#">About</a></span>
         <span><a href="#">Contact</a></span>
