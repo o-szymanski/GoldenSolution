154530fc8eb9d8081eba39f0f429d9cde4f5f0eb
o-szymanski <osk.szymanski@gmail.com>
2024-02-19 18:14:06 +0100

    add button slide to the top of page

---
+++ GoldenSolution.Website/GoldenSolution.Website/src/components/SlideToTheTop/SlideToTheTopButton.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import styles from './SlideToTheTopButton.module.css'
+
+function SlideToTheTopButton() {
+    const [slideToTheTopButton, setSlideToTheTopButton] = useState(false);
+    useEffect(()=> {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                setSlideToTheTopButton(true) 
+            }
+            else {
+                setSlideToTheTopButton(false)
+            }
+        })
+    })
+
+    const slideToTheTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        })
+    }
+
+    return <div>
+        {slideToTheTopButton && (
+            <button className={styles.SlideToTheTopButton} onClick={slideToTheTop}>^</button>
+        )}
+    </div>
+}
+
+export default SlideToTheTopButton;
