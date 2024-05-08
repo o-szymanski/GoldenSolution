cb1359c30f1167a0554779da98a12aac2c1e2d92
o-szymanski <osk.szymanski@gmail.com>
2024-05-08 12:25:36 +0200

    Generic Repository / Database / Mapping

---
+++ GoldenSolution.DAL/Models/GoldenSolutionDatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GoldenSolution.DAL.Models;
+
+public partial class GoldenSolutionDatabaseContext : DbContext
+{
+    public GoldenSolutionDatabaseContext()
+    {
+    }
+
+    public GoldenSolutionDatabaseContext(DbContextOptions<GoldenSolutionDatabaseContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=GoldenSolutionDatabase;Trusted_Connection=True;Encrypt=false;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
