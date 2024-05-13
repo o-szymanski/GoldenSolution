ac7daa0e6ec6da6aba8709ae2b5e8573d0516856
o-szymanski <osk.szymanski@gmail.com>
2024-05-14 00:23:42 +0200

    Add MediatR / Mapper / Namespace change / CQRS

---
+++ GoldenSolution.Infrastructure/Handlers/GetUserNameHandler.cs
@@ -0,0 +1,24 @@
+﻿using GoldenSolution.Core.DAL;
+using GoldenSolution.Core.DTO.Authentication;
+using GoldenSolution.Core.Function.Query;
+using GoldenSolution.Core.Mappers.AuthenticationMappers;
+using GoldenSolution.DAL.Models;
+using MediatR;
+
+namespace GoldenSolution.Infrastructure.Handlers;
+
+public class GetUserNameHandler : IRequestHandler<GetUserNameQuery, UserDto>
+{
+	private readonly IRepository<User> repository;
+
+	public GetUserNameHandler(IRepository<User> repository)
+	{
+		this.repository = repository;
+	}
+
+	public async Task<UserDto> Handle(GetUserNameQuery request, CancellationToken cancellationToken)
+	{
+		var user = await repository.GetById(request.Id);
+		return user == null ? new UserDto { Id = 0 } : UserMap.ToUserDto(user);
+	}
+}
