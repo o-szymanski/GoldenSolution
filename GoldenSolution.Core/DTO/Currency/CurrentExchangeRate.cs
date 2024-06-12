7444191e0802c7a79eed5094106c55e1d2b62de6
o-szymanski <osk.szymanski@gmail.com>
2024-06-12 14:09:43 +0200

    Add Currencies Controller

---
+++ GoldenSolution.Core/DTO/Currency/CurrentExchangeRate.cs
@@ -0,0 +1,8 @@
+﻿namespace GoldenSolution.Core.DTO.Currency;
+
+public class CurrentExchangeRate
+{
+	public string Currency { get; set; } = string.Empty;
+	public string Code { get; set; } = string.Empty;
+	public double Mid { get; set; }
+}
---
+++ GoldenSolution.Infrastructure/Handlers/GetCurrentExchangeRatesHandler.cs
@@ -0,0 +1,25 @@
+﻿using GoldenSolution.Core.DTO.Currency;
+using GoldenSolution.Core.Function.Query;
+using MediatR;
+using System.Net.Http.Json;
+
+namespace GoldenSolution.Infrastructure.Handlers;
+
+public class GetCurrentExchangeRatesHandler : IRequestHandler<GetCurrentExchangeRatesQuery, List<CurrentExchangeRates>>
+{
+	private readonly IHttpClientFactory _httpClientFactory;
+
+
+	public GetCurrentExchangeRatesHandler(IHttpClientFactory httpClientFactory)
+    {
+		_httpClientFactory = httpClientFactory;
+    }
+
+    public async Task<List<CurrentExchangeRates>> Handle(GetCurrentExchangeRatesQuery request, CancellationToken cancellationToken)
+	{
+		var client = _httpClientFactory.CreateClient("currency");
+		var result = await client.GetFromJsonAsync<List<CurrentExchangeRates>>("", cancellationToken);
+		result ??= [];
+		return result;
+	}
+}
