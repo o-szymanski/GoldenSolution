b78a390e0eff8521920150a9d50db695c65624e2
o-szymanski <osk.szymanski@gmail.com>
2024-06-16 19:27:20 +0200

    Refactor

--- GoldenSolution.Core/DAL/IRepository.cs
+++ GoldenSolution.Core/DAL/IRepository.cs
@@ -2,9 +2,11 @@
 
 public interface IRepository<T> where T : class
 {
-	Task<IEnumerable<T>> GetAll();
+	Task<IQueryable<T>> GetQueryable();
+	Task<List<T>> GetAll();
 	Task<T?> GetById(int id);
-	Task Add(T entity);
+	Task Insert(T entity);
 	Task Update(T entity);
 	Task Delete(T entity);
+	Task SaveChanges();
 }
