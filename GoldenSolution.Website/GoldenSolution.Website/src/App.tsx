c3e1758cc5c85148ce23b0d52469af5e3c2519fd
molejnik <24421260+maciekole@users.noreply.github.com>
2024-06-28 21:25:23 +0200

    29: content section with cards

--- GoldenSolution.Website/GoldenSolution.Website/src/App.tsx
+++ GoldenSolution.Website/GoldenSolution.Website/src/App.tsx
@@ -1,14 +1,16 @@
 import SlideToTheTopButton from './components/SlideToTheTop/SlideToTheTopButton';
 import Footer from "./components/Footer/Footer.tsx";
 import NavigationBar from "./components/NavigationBar/NavigationBar.tsx"
+import ContentSection from "./components/ContentSection/ContentSection.tsx";
 import './App.css'
 import React from "react";
 
 function App(): React.ReactElement {
-  return (
+    return (
     <>
       <NavigationBar />
       <SlideToTheTopButton />
+      <ContentSection />
       <Footer />
     </>
   )
