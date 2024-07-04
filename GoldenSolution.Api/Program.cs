5880aba6313e43c3e7cc1bacd4539bb85829a83b
o-szymanski <osk.szymanski@gmail.com>
2024-07-04 18:01:31 +0200

    Add Serilog / Elasticsearch / Kibana

--- GoldenSolution.Api/Program.cs
+++ GoldenSolution.Api/Program.cs
@@ -8,8 +8,28 @@ using GoldenSolution.Core.DTO.Authentication;
 using GoldenSolution.Core.Function.Query;
 using GoldenSolution.Core.Models.Currency;
 using Microsoft.AspNetCore.Diagnostics;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+	configuration.Enrich.FromLogContext()
+	.Enrich.WithMachineName()
+	.WriteTo.Console()
+	.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"] ?? string.Empty))
+	{
+		IndexFormat = $"{context.Configuration["ApplicationName"] ?? string.Empty}-{DateTime.UtcNow:yyyy-MM}",
+		AutoRegisterTemplate = true,
+		NumberOfShards = 2,
+		NumberOfReplicas = 1
+	})
+	.Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+	.ReadFrom.Configuration(context.Configuration);
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -22,7 +42,7 @@ builder.Services.AddTransient<IRequestHandler<GetCurrencyExchangeRatesQuery, Lis
 
 builder.Services.AddHttpClient("currency", client =>
 {
-	client.BaseAddress = new Uri("https://api.nbp.pl/api/exchangerates/tables/a/?format=json");
+	client.BaseAddress = new Uri(builder.Configuration["NBP"] ?? string.Empty);
 });
 
 builder.Services.AddScoped(typeof(IRepository<>), typeof(RepositoryBase<>));
@@ -41,6 +61,7 @@ app.UseExceptionHandler(error =>
 	error.Run(async context =>
 	{
 		var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+		if (contextFeature != null) Log.Logger.Error($"{contextFeature.Path} {contextFeature.Error.Message} {contextFeature.Error.StackTrace}", "Internal Server Error");
 		await context.Response.WriteAsJsonAsync(new
 		{
 			context.Response.StatusCode,
