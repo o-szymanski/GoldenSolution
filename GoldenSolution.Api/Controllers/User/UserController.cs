c6439a403680adabcd46baa8ccb6d0403b488359
o-szymanski <osk.szymanski@gmail.com>
2024-07-11 18:08:28 +0200

    Refactor / Tests / Changes

---
+++ GoldenSolution.Api/Controllers/User/UserController.cs
@@ -0,0 +1,30 @@
+﻿using GoldenSolution.Core.DTO.User;
+using GoldenSolution.Core.Function.Query;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoldenSolution.Api.Controllers.User;
+
+[ApiController]
+[Route("api/users")]
+public class UserController : ControllerBase
+{
+	private readonly IMediator _mediator;
+
+	public UserController(IMediator mediator)
+	{
+		_mediator = mediator;
+	}
+
+	[HttpGet("{id:int}")]
+	[ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	public async Task<IActionResult> GetUserNameAsync(int id)
+	{
+		var user = await _mediator.Send(new GetUserNameQuery(id));
+
+		if (user.Id == 0) return NotFound();
+
+		return Ok(user);
+	}
+}
--- GoldenSolution.Core/ExternalModels/Currency/CurrencyExchange.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GoldenSolution.Core.ExternalModels.Currency;
-
-public class CurrencyExchange
-{
-	public string Table { get; set; } = string.Empty;
-	public string No { get; set; } = string.Empty;
-	public string EffectiveDate { get; set; } = string.Empty;
-	public required List<Rates> Rates { get; set; }
-}
