10992d087120a88f453f7985a233d9c363a3bd0b
o-szymanski <osk.szymanski@gmail.com>
2024-07-19 22:49:35 +0200

    Add Service / Delete Generic Repositories / Fix

--- GoldenSolution.DAL/Models/User.cs
+++ GoldenSolution.DAL/Models/User.cs
@@ -1,16 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
+﻿namespace GoldenSolution.DAL.Models;
 
-namespace GoldenSolution.DAL.Models;
-
-[Table("User")]
 public partial class User
 {
-    [Key]
     public int Id { get; set; }
-
-    [StringLength(50)]
-    [Unicode(false)]
     public string FirstName { get; set; } = null!;
 }
