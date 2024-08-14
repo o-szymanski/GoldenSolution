5f502be58933aa4fcca1d8e344abd78b540a4800
o-szymanski <osk.szymanski.portale@gmail.com>
2024-08-15 00:02:18 +0200

    Refactor / Mapperly

--- GoldenSolution.Api/Extensions/ServiceCollectionExtensions.cs
+++ GoldenSolution.Api/Extensions/ServiceCollectionExtensions.cs
@@ -19,43 +19,43 @@ namespace GoldenSolution.Api.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddCustomServices(this IServiceCollection services)
-    {
-        services.AddTransient<IRequestHandler<GetUserNameByIdQuery, UserDto>, GetUserNameByIdHandler>();
-        services.AddTransient<IRequestHandler<GetCurrencyExchangeRatesQuery, List<CurrencyExchangeDto>>, GetCurrencyExchangeRatesHandler>();
-        services.AddTransient<IRequestHandler<GetAllCareerQuery, List<CareerDto>>, GetAllCareerHandler>();
+	public static void AddCustomServices(this IServiceCollection services)
+	{
+		services.AddTransient<IRequestHandler<GetUserNameByIdQuery, UserDto>, GetUserNameByIdHandler>();
+		services.AddTransient<IRequestHandler<GetCurrencyExchangeRatesQuery, List<CurrencyExchangeDto>>, GetCurrencyExchangeRatesHandler>();
+		services.AddTransient<IRequestHandler<GetAllCareerQuery, List<CareerDto>>, GetAllCareerHandler>();
 
-        services.AddHttpClient("currency", client =>
-        {
-            client.BaseAddress = new Uri(services.BuildServiceProvider().GetRequiredService<IConfiguration>()["NBP"] ?? string.Empty);
-        });
+		services.AddHttpClient("currency", client =>
+		{
+			client.BaseAddress = new Uri(services.BuildServiceProvider().GetRequiredService<IConfiguration>()["NBP"] ?? string.Empty);
+		});
 
-        services.AddScoped<IUserService, UserService>();
-        services.AddScoped<ICareerService, CareerService>();
+		services.AddScoped<IUserService, UserService>();
+		services.AddScoped<ICareerService, CareerService>();
 
-        services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+		services.AddMediatR(config => config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 
-        services.AddSingleton<UserMapper>();
-        services.AddSingleton<CurrencyExchangeMapper>();
-        services.AddSingleton<CareerMapper>();
-    }
+		services.AddSingleton<IUserMapper, UserMapper>();
+		services.AddSingleton<ICurrencyExchangeMapper, CurrencyExchangeMapper>();
+		services.AddSingleton<ICareerMapper, CareerMapper>();
+	}
 
-    public static void ConfigureCors(this IServiceCollection services)
-    {
-        services.AddCors(options =>
-        {
-            options.AddPolicy("AllowSpecificOrigin", builder =>
-            {
-                builder.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod();
-            });
-        });
-    }
+	public static void ConfigureCors(this IServiceCollection services)
+	{
+		services.AddCors(options =>
+		{
+			options.AddPolicy("AllowSpecificOrigin", builder =>
+			{
+				builder.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod();
+			});
+		});
+	}
 
-    public static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
-    {
-        services.AddDbContext<GoldenSolutionDatabaseContext>(options =>
-        {
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection") ?? string.Empty);
-        });
-    }
+	public static void AddDatabase(this IServiceCollection services, IConfiguration configuration)
+	{
+		services.AddDbContext<GoldenSolutionDatabaseContext>(options =>
+		{
+			options.UseSqlServer(configuration.GetConnectionString("DefaultConnection") ?? string.Empty);
+		});
+	}
 }
